4af1b28b189ed0ff8ea8dcf9d98137bc96214862
Sukojay <sukojay373@gmail.com>
2023-04-05 03:24:58 +0300

    added checkAverage

--- Math/checkAverage.js
+++ Math/checkAverage.js
@@ -1,6 +1,23 @@
 // goal is to check if any of the 3 numbers is equal to the average of other 2
-// difficulty: easy
+// difficulty: very easy
 
-let a = 3;
-let b = 2; 
-let c = 4;
+let a = 6;
+let b = 9; 
+let c = 12;
+
+// average = (a+b)/coefficient
+// coefficient is the amount of numbers in numerator, here 2 numbers
+function checkAverage(a, b, c){
+  if((a+b)/2 === c) 
+    console.log(`${c} is the average of ${a} and ${b}`);
+  
+  else if((a+c)/2 === b) 
+    console.log(`${b} is the average of ${a} and ${c}`);
+
+  else if((b+c)/2 === a)
+    console.log(`${a} is the average of ${b} and ${c}`);
+
+  else 
+    console.log('non is average of any');
+}
+checkAverage(a, b, c);
