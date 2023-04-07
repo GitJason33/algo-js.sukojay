55ec95ccd9d2fb338dfc46d959ee7ab04d751469
Sukojay <sukojay373@gmail.com>
2023-04-07 20:04:14 +0300

    added negative to powerV2.0

--- Math/powerByAddition.js
+++ Math/powerByAddition.js
@@ -1,5 +1,5 @@
 // goal is to find the power of a base, but multiplication is restricted
-// difficulty: easy +
+// difficulty: medium
 
 /*
 to solve, reminder of multiplication: 
@@ -27,29 +27,41 @@ and we finish. but wait, that's not exponent times, that's 'exponent-1' times!
 and we can see that each time we finish an addition, we take the result and use
 it as a new base each time. we call it a 'term' in an expression. here updating.
 */
-let base = 3;
-let exponent = 4;
+let base = -3;
+let exponent = -4;
 
-const power = (base, exponent = base)=> {
-  // you have to define this manually as result starts at 0 not 1
-  if(exponent === 0 || base === 1) return 1;
-  if(base === 0) return 0;
+console.log(power(base, exponent));
 
-  let result = 0;
+function power(base, exponent){
+  if(base === 0) return 0; // 0 is 0
+  // n^1 = n no matter what
+  if(exponent === 1) return base;
+
+  // n^0 = 1 no matter what
+  if(exponent === 0) return 1;
+
+  let result = 1;
   let term = base;
 
-  // the whole thing is exponent-1 times
-  for(let i = 0; i < exponent-1; i++){
+  // the whole thing is (exponent-1) times
+  for(let i = 0; i < Math.abs(exponent)-1; i++){
     // reset the result each time before you go on
     result = 0;
 
-    // each term calculation is base times
-    for(let j = 0; j < base; j++)
+    // each term calculation is base times (base not negative!)
+    for(let j = 0; j < Math.abs(base); j++)
       result += term;
 
     // assign the result to the term 'after' performing the addition
     term = result;
   }
+  // check if the exponent is negative, it's a go backwards: 2^-2 = 1/(2^2)
+  if(exponent < 0) {
+    if(exponent === -1) return -1/result;
+    return 1/result;
+  }
+
+  // check negativity: if the exponent is even, the sign is opposed
+  if(base < 0 && exponent % 2 === 0) return result*-1;
   return result;
 }
-console.log(power(base, exponent));
