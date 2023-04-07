73c389a6816372599a0c171c257c6df623194abb
Sukojay <sukojay373@gmail.com>
2023-04-07 19:29:10 +0300

    fixed powerV1.0

--- Math/power.js
+++ Math/power.js
@@ -1,14 +1,13 @@
 // goal is to calculate the power of a number using an algorithm, built in functions are restricted
 // difficulty: easy
 
-let base = 5;
-let exponent = 8;
+let base = -5;
+let exponent = 9;
 
-const power = (base, exponent = base) => {
+const power = (base, exponent) => {
   let result = 1;
   for(let i = 1; i <= exponent; i++)
     result *= base;
   return result;
 }
 console.log(`${base}^${exponent} = ${power(base, exponent)}`);
-console.log(`${base}^${base} = ${power(base)}`);
