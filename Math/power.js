2fdfa70defd1298c7318f8cf136cd2ef267223b9
Sukojay <sukojay373@gmail.com>
2023-04-07 21:12:45 +0300

    added negative to powerV1.0

--- Math/power.js
+++ Math/power.js
@@ -2,12 +2,23 @@
 // difficulty: easy
 
 let base = -5;
-let exponent = 9;
+let exponent = -2;
+console.log(`${base}^${exponent} = ${power(base, exponent)}`);
 
-const power = (base, exponent) => {
+
+function power (base, exponent) {
+  if(base === 1) return 1;
+  if(base === 0) return 0;
+
   let result = 1;
-  for(let i = 1; i <= exponent; i++)
+  for(let i = 0; i < Math.abs(exponent); i++)
     result *= base;
+  
+  // check if exponent is negative, that will go backwards: -2^-2 = -1/4
+  if(exponent < 0){
+    if(exponent === -1) return 1/base;
+    else if(base < 0) return -1/result;
+    else return 1/result;
+  }
   return result;
 }
-console.log(`${base}^${exponent} = ${power(base, exponent)}`);
