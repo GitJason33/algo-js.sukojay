3556bd05e3949cc845875837e6cbdc2d046e4f98
Sukojay <sukojay373@gmail.com>
2023-04-08 19:56:13 +0300

    added decrypt

--- String/decrypt.js
+++ String/decrypt.js
@@ -12,8 +12,8 @@ console.log("Text initially: " + important['text']);
 encrypt(important);
 console.log("Text encrypted: " + important['text']);
 
-// decrypt(important);
-// console.log("Text decrypted: " + important['text']);
+decrypt(important);
+console.log("Text decrypted: " + important['text']);
 
 // I changed to using an object
 function encrypt(toEncrypt){
@@ -23,17 +23,27 @@ function encrypt(toEncrypt){
 
   for(let i = 0; i < len; i++){
     let rand = Math.floor(Math.random() * 1000) + 1; 
-
     let ch = rand + toEncrypt['text'].charCodeAt(i);
-
     result += String.fromCharCode(ch);
-
     toEncrypt['security'] += rand + " ";
   }
   toEncrypt['text'] = result;
 }
 
-// decrypt the text
-function decrypt(encrypted){
+// decrypt the text: it's like using a key to open the vault (security key)
+function decrypt(toDecrypt){
+  let len = toDecrypt['text'].length;
   
+  // check if the text has a security key
+  if(toDecrypt['security'] == '') return;
+  let result = "";
+  let securityNumbers = toDecrypt['security'].split(" ");
+
+  for(let i = 0; i < len; i++){
+    // security number represents the random number added when encrypted
+    let ch = toDecrypt['text'].charCodeAt(i) - securityNumbers[i];
+    result += String.fromCharCode(ch);
+  }
+  toDecrypt['security'] = ""; // delete the security key
+  toDecrypt['text'] = result;
 }
