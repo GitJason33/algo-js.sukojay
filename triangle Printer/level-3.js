b205fd5f042a9e411dc31cde945cda0c44d9aa60
Sukojay <sukojay373@gmail.com>
2023-04-10 15:50:21 +0300

    finished pattern printing

--- triangle Printer/level-3.js
+++ triangle Printer/level-3.js
@@ -1,9 +1,11 @@
 // goal here is to draw even harder shapes which are: infinity-like symbol with crowns
-// difficulty: challenging (left: easy, right: medium)
+// difficulty: challenging
 
-let n = 3; 
-console.log(crown(n));
+let n = 5; 
+// console.log(crown(n));
 // console.log(reverseCrown(n));
+console.log(infinity(n));
+
 
 /* infinity like symbol - let's divide the problem into smaller steps
 *                   *
@@ -14,13 +16,14 @@ console.log(crown(n));
 * * * * * * * * * * *
 */
 // let's draw each part as a crown
+
 // first, notice that we draw stars, then spaces, then stars again in a mirror way?
-// confusing enough, the spaces are counted: rightSpaces + leftSpaces - 1: middle 1 is an intersection
+// confusing enough, the spaces are counted: rightSpaces + leftSpaces - 2: middle 1 is an intersection, and 1 extra space is counted
 function crown(num, symbol = "*"){
   let output = "";
 
   // equation of spaces amount
-  let spaceCount = (num - 1)*4 - 1;
+  let spaceCount = (num - 1)*4 - 2;
 
   // row by row
   for(let row = 1; row <= num; row++){
@@ -29,10 +32,10 @@ function crown(num, symbol = "*"){
       output += symbol + " ";
 
     // spaces are calculated using the equation above and times 2 again to add all needed spaces
-    for(let spaces = 1; spaces < spaceCount; spaces++)
+    for(let spaces = 1; spaces <= spaceCount; spaces++)
       output += " ";
 
-    // now draw the second side stars, same as the first side ones
+    // now draw the second side stars, resembles to level-2 triangle
     for(let stars = 1; stars <= row; stars++)
       output += symbol + " ";
 
@@ -54,8 +57,36 @@ function crown(num, symbol = "*"){
 * *               * *
 *                   *
 */
+// currentSpace is the max number of spaces that were counted. counting it from 1 above spcaceCount
 function reverseCrown(num, symbol = "*"){
+  let output = "";
+  let spaceCount = (num - 1)*4 - 2;
+  let currentSpace = spaceCount + 2; // count an extra space because of star printing on left side
+  
+  for(let row = 1; row <= num; row++){
+    // first, notice that left resembles to level-1
+    for(let stars = row; stars <= num; stars++)
+      output += symbol + " ";
+    
+    // now, draw the spaces like above using the equation, but ascending instead
+    for(let spaces = currentSpace; spaces < spaceCount; spaces++)
+      output += " ";
 
+    // now draw the second side stars, resemble to level-2's reverse triangle
+    for(let stars = row; stars <= num; stars++)
+      output += symbol + " ";
+
+    // avoid an extra generation of star at 1st line
+    if(row === 1) output = output.slice(0, output.length - 2);
+    
+    // jump 1 line after finishing 1 row
+    output += '\n';
+
+    // remove 4 spaces from current space each row you visit (2 for each triangle)
+    currentSpace -= 4;
+  }
+
+  return output;
 }
 
 /* infinity like symbol
@@ -72,8 +103,26 @@ function reverseCrown(num, symbol = "*"){
 *                   *
 */
 function infinity(num, symbol = "*"){
-  let output = crown(num, symbol);
+  // jump 1 line before going on, as we removed a new line in crown()
+  let output = crown(num, symbol) + '\n';
 
-  // first, we are working on double the rows-1. but notice that the shape resembles to a crown as well
-  // for(let row = 1; row <)
+  // draw the reverse crown starting from row 2
+  let spaceCount = ((num - 1)*4 - 2) - 4; // starting row 2 not 1, we start 4 steps before
+  let currentSpace = spaceCount - 2; 
+  
+  for(let row = 2; row <= num; row++){
+    for(let stars = row; stars <= num; stars++)
+      output += symbol + " ";
+    
+    for(let spaces = currentSpace; spaces < spaceCount; spaces++)
+      output += " ";
+
+    for(let stars = row; stars <= num; stars++)
+      output += symbol + " ";
+
+    output += '\n';
+    currentSpace -= 4;
+  }
+
+  return output;
 }
